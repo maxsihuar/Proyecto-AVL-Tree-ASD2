a2ccc440a68edbc8af54d9c8dd4e28266d957664
Marco <marcoantonioaroniperez@gmail.com>
2026-06-14 17:39:48 -0500

    impleentado métodos y atributos de las clases

--- Clases/cLector.cs
+++ Clases/cLector.cs
@@ -8,5 +8,52 @@ namespace Clases
 {
     internal class cLector
     {
+        public string Id { get; set; }
+        public string AP { get; set; }
+        public string AM { get; set; }
+        public string Nombre { get; set; }
+        public string Condicion { get; set; }
+        public string Email { get; set; }
+
+        public void AgregarLector(string id, string ap, string am, string nombre, string condicion, string email)
+        {
+            Id = id;
+            AP = ap;
+            AM = am;
+            Nombre = nombre;
+            Condicion = condicion;
+            Email = email;
+        }
+        public void MostrarLector()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Apellido Paterno: " + AP);
+            Console.WriteLine("Apellido Materno: " + AM);
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Condición: " + Condicion);
+            Console.WriteLine("Email: " + Email);
+        }
+        public void ModificarLector(string id, string ap, string am, string nombre, string condicion, string email)
+        {
+            Id = id;
+            AP = ap;
+            AM = am;
+            Nombre = nombre;
+            Condicion = condicion;
+            Email = email;
+        }
+        public void EliminarLector()
+        {
+            Id = null;
+            AP = null;
+            AM = null;
+            Nombre = null;
+            Condicion = null;
+            Email = null;
+        }
+        public bool BuscarLector(string id)
+        {
+            return Id == id;
+        }
     }
 }
--- Clases/cLibro.cs
+++ Clases/cLibro.cs
@@ -1,7 +1,55 @@
-﻿namespace Clases
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Clases
+{
+    internal class CLibro
+    {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Anio { get; set; }
+        public string Especialidad { get; set; }
+        public void AgregarLibro(string id, string titulo, string autor, string anio, string especialidad)
+        {
+            Id = id;
+            Titulo = titulo;
+            Autor = autor;
+            Anio = anio;
+            Especialidad = especialidad;
+        }
+        public void MostrarLibro()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Titulo: " + Titulo);
+            Console.WriteLine("Autor: " + Autor);
+            Console.WriteLine("Año: " + Anio);
+            Console.WriteLine("Especialidad: " + Especialidad);
+        }
+        public void ModificarLibro(string id, string titulo, string autor, string anio, string especialidad)
+        {
+            Id = id;
+            Titulo = titulo;
+            Autor = autor;
+            Anio = anio;
+            Especialidad = especialidad;
+        }
+        public void EliminarLibro()
+        {
+            Id = null;
+            Titulo = null;
+            Autor = null;
+            Anio = null;
+            Especialidad = null;
+        }
+        public bool BuscarLibro(string id)
+        {
+            return Id == id;
+        }
     }
+
 }
+
